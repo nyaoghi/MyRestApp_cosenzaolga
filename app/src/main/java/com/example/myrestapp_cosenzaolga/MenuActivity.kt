f2c53a598445abcd3e5aa53892a84274563e1de0
Cosenza Olga <olgacosenza06@gmail.com>
2025-06-05 22:41:54 +0200

    riformattazione delle activity e modifica dei button

--- app/src/main/java/com/example/myrestapp_cosenzaolga/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp_cosenzaolga/MenuActivity.kt
@@ -2,22 +2,24 @@ package com.example.myrestapp_cosenzaolga
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
 
-        var mioIntent : Intent = Intent(this, movie::class.java)
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        findViewById<Button>(R.id.SearchFilm).setOnClickListener {
+            startActivity(Intent(this, SearchFilmActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.RentFilm).setOnClickListener {
+            startActivity(Intent(this, WebViewActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.BookCinema).setOnClickListener {
+            startActivity(Intent(this, ThirdActivity::class.java))
         }
     }
 }
